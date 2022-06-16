dee2a02ebd2abd179c90de22c1d19d46ee1ad39a
jianglan <250455653@qq.com>
2022-06-16 20:10:33 +0800

    deploy

---
+++ precache-manifest.c23aecefee56c3cf71315a55bd31e7d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd89dc6db0cd72e0f8251d4c390ba815",
+    "url": "/WIMM-Deployment/QRCode.png"
+  },
+  {
+    "revision": "6ff9d4c63c50e7c69715",
+    "url": "/WIMM-Deployment/css/app.c2034c59.css"
+  },
+  {
+    "revision": "fe034bc085b3f71d520e43465672991a",
+    "url": "/WIMM-Deployment/index.html"
+  },
+  {
+    "revision": "6ff9d4c63c50e7c69715",
+    "url": "/WIMM-Deployment/js/app.9c579b31.js"
+  },
+  {
+    "revision": "c9fad951f32bcb4ad8aa",
+    "url": "/WIMM-Deployment/js/chunk-vendors.accd427c.js"
+  },
+  {
+    "revision": "6f322357411d7f729cb718de28687581",
+    "url": "/WIMM-Deployment/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/WIMM-Deployment/robots.txt"
+  }
+]);
